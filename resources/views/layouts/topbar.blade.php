df1f1f023dbf1545ddf8da617942e40ca2efdca8
Abi Anbiya <anbiya@students.unnes.ac.id>
2024-06-19 09:14:59 +0700

    fix: skip simpanseu

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -242,7 +242,7 @@
                 <div class="dropdown ms-sm-3 header-item topbar-user">
                     <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="d-flex align-items-center">
-                            <img class="rounded-circle header-profile-user" src="{{ asset('build/images/brands/mail_chimp.png') }}" alt="Header Avatar">
+                            <img class="rounded-circle header-profile-user" src="{{ asset('build/images/users/user-dummy-img.jpg') }}" alt="Header Avatar">
                             <span class="text-start ms-xl-2">
                                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::user()->name }}</span>
                                 <span class="d-none d-xl-block ms-1 fs-13 text-reset user-name-sub-text">{{ session('active_role_name') }}</span>
