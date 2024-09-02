873cc93d6497e74aeb0f9bd23b1c6596ee329132
Abi Anbiya <anbiya@students.unnes.ac.id>
2024-09-02 22:53:22 +0700

    fix: config laravel

--- config/laralag.php
+++ config/laralag.php
@@ -16,4 +16,7 @@ return [
 	'dashboard_blade' => '',
 	'custom_login_blade' => '',
 	'custom_register_blade' => '',
+	'app_name' => env('APP_NAME', 'Laralag'),
+	'app_short_name' => env('APP_SHORT_NAME', 'LAG'),
+	'theme_customizer_enabled' => env('THEME_CUSTOMIZER_ENABLED', false),
 ];
