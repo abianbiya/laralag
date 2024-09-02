330fa430e7e19e02bf9f673f14291f68afdee1a8
Abi Anbiya <anbiya@students.unnes.ac.id>
2024-09-02 22:52:57 +0700

    feat: by config theme customizer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8" />
     <title> @yield('title') | {{ config('app.name') }} </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Laralag CRUD Generator and Access Management" name="description" />
+    <meta content="Abianbiya" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico') }}">
     @include('Laralag::layouts.head-css')
@@ -37,7 +37,9 @@
     </div>
     <!-- END layout-wrapper -->
 
-    @include('Laralag::layouts.customizer')
+    @if(config('laralag.theme_customizer_enabled'))
+        @include('Laralag::layouts.customizer')
+    @endif
     <!-- JAVASCRIPT -->
     @include('Laralag::layouts.vendor-scripts')
     @stack('js')
